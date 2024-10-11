e99480b1f8c32e8d9430ffa2bcb0ed187953a581
rtbn12 <136724287+rtbn12@users.noreply.github.com>
2024-10-11 08:00:49 +0300

    Добавьте файлы проекта.

---
+++ laba4dz/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<locale.h>
+#define      APPLEPRICE       1
+#define      PEARSPRICE       2
+#define      ORANGESPRICE     3
+int sum();
+
+int main() {
+	setlocale(LC_ALL,"RUS");
+
+	return 0;
+}
+int sum() {
+	printf("¬ведите,пожалуйста количество проданных €блок,груш и апельсиноы,\nа € посчитаю сумму,на которую вы продали фруктов в каждый из двух дней!");
+
+	return 0;
+};
